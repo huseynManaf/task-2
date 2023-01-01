b6f69c57a539895be683339602c7d845f7509199
huseynManaf <huseynim@code.edu.az>
2023-01-01 03:22:38 -0800

    init

---
+++ task izah/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace task_izah
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task 1
+            //int n = int.Parse(Console.ReadLine());
+            //if (n >= 1 && n <= 1188)
+
+            //{
+            //    int il = n / 12;
+            //    int ay = n % 12;
+            //    Console.WriteLine($"{il} il ve {ay} ay ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("1- 1188 arasi reqem qeyd edin");
+            //    //    }
+            #endregion
+
+
+            #region task 2
+            //int n = int.Parse(Console.ReadLine());
+            //int zeroCount = 0;
+            //int tmp = n;
+
+            //while (tmp != 0)
+            //{
+            //    if (tmp % 10 == 0)
+
+            //        tmp /= 10;
+
+            //    Console.WriteLine($"{n}-ideki sifirlari sayi= {zeroCount}");
+            //}
+            #endregion
+
+            #region task 3
+            //decimal n =int.Parse(Console.ReadLine());
+            //int real = (int)n;
+            //decimal kesr = n - real;
+            //decimal x = (real / 1000M);
+            //decimal reverse = x + kesr * 1000;
+            //Console.WriteLine($"reverse");
+            #endregion
+        }
+    }
+}
